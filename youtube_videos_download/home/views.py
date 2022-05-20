c95ce2dae56472b66fe2b2e36db5a867b2f960b3
gajanan <rajputgajanan50@gmail.com>
2022-05-20 15:25:42 +0530

    YouTube_video_download: - Added contact us - Added Loader

--- youtube_videos_download/home/views.py
+++ youtube_videos_download/home/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import youtube_dl
+from django.core.mail import send_mail
+from django.conf import settings
+from smtplib import SMTPException
+
 
 
 # Create your views here.
@@ -66,4 +70,20 @@ def contact_us(request):
         such as the HTTP method, the URL, the headers, and the body
         :return: The contact.html page is being returned.
     """
+    if request.POST:
+        try:
+            send_mail(
+                subject=request.POST['subject'],
+                message=request.POST['message'],
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[request.POST['email'],]
+            )
+            return JsonResponse({"success":'Success! Your message has been sent to us.'}, status=200)
+        except SMTPException as e:
+            return JsonResponse({"error": f'There was an error sending an email{e}'}, status=400)
     return render(request, 'contact.html')
+
+
+
+def error_404_view(request, exception):
+    return render(request, '404.html')
